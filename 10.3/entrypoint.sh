1a7a3f355b45945c306fc0b8196ec0e5dd4fcf15
h-otter <h-otter@outlook.jp>
2017-10-26 02:30:41 +0900

    try not to use wrep-new-cluster

--- 10.3/entrypoint.sh
+++ 10.3/entrypoint.sh
@@ -3,9 +3,4 @@
 
 ARGS="--wsrep-cluster-name=$POD_SERVICE_NAME --wsrep-node-address=$POD_IP"
 
-if [ -z $(dig +short +search $POD_SERVICE_NAME | grep -v $POD_IP) ]
-then
-  /docker-entrypoint.sh mysqld $ARGS --wsrep-cluster-address=gcomm:// --wsrep-new-cluster
-else
-  /docker-entrypoint.sh mysqld $ARGS --wsrep-cluster-address=gcomm://`dig +short +search $POD_SERVICE_NAME | grep -v $POD_IP | paste -s -d ',' -`
-fi
+/docker-entrypoint.sh mysqld $ARGS --wsrep-cluster-address=gcomm://`dig +short +search $POD_SERVICE_NAME | grep -v $POD_IP | paste -s -d ',' -`
