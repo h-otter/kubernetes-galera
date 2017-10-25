bff26057cd1ee19c7004751b04936fdd29df90a1
h-otter <h-otter@outlook.jp>
2017-10-26 00:47:41 +0900

    check statefulset

--- 10.3/entrypoint.sh
+++ 10.3/entrypoint.sh
@@ -3,9 +3,9 @@
 
 ARGS="--wsrep-cluster-name=$POD_SERVICE_NAME --wsrep-node-address=$POD_IP"
 
-if [ -z $(dig +short +search $POD_SERVICE_NAME) ]
+if [ -z $(dig +short +search $POD_SERVICE_NAME | grep -v $POD_IP) ]
 then
   /docker-entrypoint.sh mysqld $ARGS --wsrep-new-cluster
 else
-  /docker-entrypoint.sh mysqld $ARGS --wsrep-cluster-address=gcomm://`dig +short +search $POD_SERVICE_NAME | tr '\n' ','`
+  /docker-entrypoint.sh mysqld $ARGS --wsrep-cluster-address=gcomm://`dig +short +search $POD_SERVICE_NAME | grep -v $POD_IP | tr '\n' ','`
 fi
