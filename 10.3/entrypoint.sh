7a33e12a753751c9271fe5bb9e21d74b2aaa7c08
h-otter <h-otter@outlook.jp>
2017-10-25 22:03:19 +0900

    maybe work well

--- 10.3/entrypoint.sh
+++ 10.3/entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+cluster_node=`dig +short +search $POD_SERVICE_NAME | tr '\n' ','`
+
+echo "[galera]" >> /etc/mysql/conf.d/generated_cluster.cnf
 echo "wsrep_cluster_name=$POD_SERVICE_NAME" >> /etc/mysql/conf.d/generated_cluster.cnf
-echo "wsrep_cluster_address=gcomm://`dig +short +search galera | tr '\n' ','`" >> /etc/mysql/conf.d/generated_cluster.cnf
+echo "wsrep_cluster_address=gcomm://$cluster_node" >> /etc/mysql/conf.d/generated_cluster.cnf
 echo "wsrep_node_address=$POD_IP" >> /etc/mysql/conf.d/generated_cluster.cnf
 
 if [ $(dig +short +search galera | wc -l) = 1 ]
