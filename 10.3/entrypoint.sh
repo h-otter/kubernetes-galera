04ad4a93dbd01dd5c2c9c89bbc69c80b5b9bff0e
h-otter <h-otter@outlook.jp>
2017-10-25 23:29:46 +0900

    pull always and change entrypoint

--- 10.3/entrypoint.sh
+++ 10.3/entrypoint.sh
@@ -5,7 +5,7 @@ ARGS="--wsrep-cluster-name=$POD_SERVICE_NAME --wsrep-node-address=$POD_IP"
 
 if [ $(dig +short +search galera | wc -l) = 1 ]
 then
-  mysqld $ARGS --wsrep-new-cluster
+  /docker-entrypoint.sh mysqld $ARGS --wsrep-new-cluster
 else
-  mysqld $ARGS --wsrep-cluster-address=gcomm://`dig +short +search $POD_SERVICE_NAME | tr '\n' ','`
+  /docker-entrypoint.sh mysqld $ARGS --wsrep-cluster-address=gcomm://`dig +short +search $POD_SERVICE_NAME | tr '\n' ','`
 fi
