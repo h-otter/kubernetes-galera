fa4a3d38adb68bce55eefc0db1ec6692550b4d6d
h-otter <h-otter@outlook.jp>
2017-10-26 00:04:58 +0900

    swap line

--- 10.3/entrypoint.sh
+++ 10.3/entrypoint.sh
@@ -3,9 +3,9 @@
 
 ARGS="--wsrep-cluster-name=$POD_SERVICE_NAME --wsrep-node-address=$POD_IP"
 
-if [ $(dig +short +search galera) ]
+if [ -n $(dig +short +search galera) ]
 then
-  /docker-entrypoint.sh mysqld $ARGS --wsrep-new-cluster
-else
   /docker-entrypoint.sh mysqld $ARGS --wsrep-cluster-address=gcomm://`dig +short +search $POD_SERVICE_NAME | tr '\n' ','`
+else
+  /docker-entrypoint.sh mysqld $ARGS --wsrep-new-cluster
 fi
